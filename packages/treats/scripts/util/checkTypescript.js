a22d0268bda49d8e0530402ee4ca5b111051cd5c
martinock <martino.khuangga@tokopedia.com>
2019-01-04 01:51:28 +0700

    Fix overwrite tsconfig.json when config present

--- packages/treats/scripts/util/checkTypescript.js
+++ packages/treats/scripts/util/checkTypescript.js
@@ -15,12 +15,12 @@ const isTSFileExists = () => {
     if (tsFiles.length > 0) {
         logger("warn", "Typescript files were found in your projects\n");
         if (!fs.existsSync(path.resolve(ROOT_PATH, "./tsconfig.json"))) {
-            logger("warn", "Generating tsconfig.json\n")
+            logger("warn", "Generating tsconfig.json\n");
+            fs.writeFileSync(path.resolve(ROOT_PATH, "./tsconfig.json"), JSON.stringify({}, null, 4), err => {
+                logger("error", err.stack || err);
+            });
         }
-
-        fs.writeFileSync(path.resolve(ROOT_PATH, "./tsconfig.json"), JSON.stringify({}, null, 4), err => {
-            logger("error", err.stack || err);
-        });
+        
         return true;
     }
 
