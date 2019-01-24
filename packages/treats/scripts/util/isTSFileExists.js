47d5f626812ab7069deaa78ee18c3079a598582e
martinock <martino.khuangga@tokopedia.com>
2019-01-24 14:13:02 +0700

    Remove unnecessary logging and tsconfig generator in isTSFileExists

--- packages/treats/scripts/util/isTSFileExists.js
+++ packages/treats/scripts/util/isTSFileExists.js
@@ -1,7 +1,5 @@
-const fs = require("fs-extra"),
-    glob = require("glob"),
+const glob = require("glob"),
     path = require("path"),
-    logger = require("./logger"),
     ROOT_PATH = process.cwd();
 
 /**
@@ -12,18 +10,6 @@ const isTSFileExists = () => {
     const tsFiles = glob.sync("**/*.*(ts|tsx)", { cwd: path.resolve(ROOT_PATH, "./src") });
 
     if (tsFiles.length > 0) {
-        logger("warn", "Typescript files were found in your projects\n");
-        if (!fs.existsSync(path.resolve(ROOT_PATH, "./tsconfig.json"))) {
-            logger("warn", "Generating tsconfig.json\n");
-            fs.writeFileSync(
-                path.resolve(ROOT_PATH, "./tsconfig.json"),
-                JSON.stringify({}, null, 4),
-                err => {
-                    logger("error", err.stack || err);
-                }
-            );
-        }
-
         return true;
     }
 
